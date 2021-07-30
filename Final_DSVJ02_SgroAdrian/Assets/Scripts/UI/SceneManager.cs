1faade7d57fd51564874a21470e2e1a3f9a9ab95
Adrian <Owinmowe@gmail.com>
2021-07-30 14:46:27 -0300

    Added scene manager scene change.

---
+++ Final_DSVJ02_SgroAdrian/Assets/Scripts/UI/SceneManager.cs
@@ -0,0 +1,100 @@
+﻿namespace MarsArena
+{
+
+    using System.Collections;
+    using UnityEngine;
+
+    public class SceneManager : MonoBehaviourSingleton<SceneManager>
+    {
+
+        [SerializeField] float minTimeToLoadScene = 1f;
+        [SerializeField] UI.UILoadingScreen uI_LoadingScreen = null;
+        int lastSessionScore = 0;
+        int lastSessionTime = 0;
+
+        public void LoadSceneAsync(string sceneName)
+        {
+            StartCoroutine(AsynchronousLoadWithFake(sceneName));
+        }
+
+        IEnumerator AsynchronousLoadWithFake(string scene)
+        {
+            float loadingProgress = 0;
+            float timeLoading = 0;
+
+            yield return null;
+
+            AsyncOperation ao = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(scene);
+            uI_LoadingScreen.FadeWithWhiteScreen();
+            uI_LoadingScreen.LockFade();
+            ao.allowSceneActivation = false;
+
+            while (!ao.isDone)
+            {
+                timeLoading += Time.deltaTime;
+                loadingProgress = ao.progress + 0.1f;
+                loadingProgress = loadingProgress * timeLoading / minTimeToLoadScene;
+
+                // Se completo la carga
+                if (loadingProgress >= 1)
+                {
+                    ao.allowSceneActivation = true;
+                    uI_LoadingScreen.UnlockFade();
+                }
+                yield return null;
+            }
+
+        }
+
+        public void SetLastSessionTime(int time)
+        {
+            lastSessionTime = time;
+        }
+
+        public int GetLastSessionTime()
+        {
+            return lastSessionTime;
+        }
+
+        public void SetLastSessionScore(int score)
+        {
+            lastSessionScore = score;
+        }
+
+        public int GetLastSessionScore()
+        {
+            return lastSessionScore;
+        }
+
+        public void WhiteScreenUnfade()
+        {
+            uI_LoadingScreen.WhiteScreenUnfade();
+        }
+
+        public void FakeLoad(float time)
+        {
+            StartCoroutine(FakeLoadingWithWhiteScreen(time));
+        }
+
+        public void FakeLoad(float time, string text)
+        {
+            StartCoroutine(FakeLoadingWithWhiteScreen(time, text));
+        }
+
+        IEnumerator FakeLoadingWithWhiteScreen(float time)
+        {
+            uI_LoadingScreen.FadeWithWhiteScreen();
+            uI_LoadingScreen.LockFade();
+            yield return new WaitForSecondsRealtime(time);
+            uI_LoadingScreen.UnlockFade();
+        }
+
+        IEnumerator FakeLoadingWithWhiteScreen(float time, string text)
+        {
+            uI_LoadingScreen.FadeWithWhiteScreen(text);
+            uI_LoadingScreen.LockFade();
+            yield return new WaitForSecondsRealtime(time);
+            uI_LoadingScreen.UnlockFade();
+        }
+    }
+}
---
+++ Final_DSVJ02_SgroAdrian/Assets/Scripts/UI/UILoadingScreen.cs
@@ -0,0 +1,84 @@
+﻿namespace MarsArena
+{
+    namespace UI
+    {
+
+        using System.Collections;
+        using UnityEngine;
+        using UnityEngine.UI;
+        using TMPro;
+
+        public class UILoadingScreen : MonoBehaviour
+        {
+            [SerializeField] Image whiteScreen = null;
+            [SerializeField] TextMeshProUGUI textComponent = null;
+
+            bool canFadeOut = true;
+
+            public void FadeWithWhiteScreen()
+            {
+                StopAllCoroutines();
+                textComponent.text = "";
+                StartCoroutine(whiteScreenFade());
+            }
+
+            public void FadeWithWhiteScreen(string text)
+            {
+                StopAllCoroutines();
+                textComponent.text = text;
+                StartCoroutine(whiteScreenFade());
+            }
+
+            IEnumerator whiteScreenFade()
+            {
+                while (whiteScreen.color.a + Time.deltaTime < 1)
+                {
+                    whiteScreen.color = new Color(whiteScreen.color.r, whiteScreen.color.g, whiteScreen.color.b, whiteScreen.color.a + Time.deltaTime);
+                    textComponent.color = new Color(textComponent.color.r, textComponent.color.g, textComponent.color.b, textComponent.color.a + Time.deltaTime);
+                    yield return null;
+                }
+                whiteScreen.color = Color.white;
+                textComponent.color = Color.white;
+                while (!canFadeOut)
+                {
+                    yield return null;
+                }
+                while (whiteScreen.color.a - Time.deltaTime > 0)
+                {
+                    whiteScreen.color = new Color(whiteScreen.color.r, whiteScreen.color.g, whiteScreen.color.b, whiteScreen.color.a - Time.deltaTime);
+                    textComponent.color = new Color(textComponent.color.r, textComponent.color.g, textComponent.color.b, whiteScreen.color.a - Time.deltaTime);
+                    yield return null;
+                }
+                whiteScreen.color = new Color(whiteScreen.color.r, whiteScreen.color.g, whiteScreen.color.b, 0);
+                textComponent.color = new Color(textComponent.color.r, textComponent.color.g, textComponent.color.b, 0);
+            }
+
+            public void LockFade()
+            {
+                canFadeOut = false;
+            }
+
+            public void UnlockFade()
+            {
+                canFadeOut = true;
+            }
+
+            public void WhiteScreenUnfade()
+            {
+                StartCoroutine(WhiteScreenUnfadeCoroutine());
+            }
+
+            IEnumerator WhiteScreenUnfadeCoroutine()
+            {
+                while (whiteScreen.color.a - Time.deltaTime > 0)
+                {
+                    whiteScreen.color = new Color(whiteScreen.color.r, whiteScreen.color.g, whiteScreen.color.b, whiteScreen.color.a - Time.deltaTime);
+                    textComponent.color = new Color(textComponent.color.r, textComponent.color.g, textComponent.color.b, whiteScreen.color.a - Time.deltaTime);
+                    yield return null;
+                }
+                whiteScreen.color = new Color(whiteScreen.color.r, whiteScreen.color.g, whiteScreen.color.b, 0);
+                textComponent.color = new Color(textComponent.color.r, textComponent.color.g, textComponent.color.b, 0);
+            }
+        }
+    }
+}
