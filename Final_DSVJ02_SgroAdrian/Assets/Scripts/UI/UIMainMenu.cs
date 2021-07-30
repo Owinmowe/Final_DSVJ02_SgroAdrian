a0975626ce391383d16de53f60fc915b4a550937
Adrian <Owinmowe@gmail.com>
2021-07-30 14:46:14 -0300

    Added splash screen animations.

--- Final_DSVJ02_SgroAdrian/Assets/Scripts/UI/UIMainMenu.cs
+++ Final_DSVJ02_SgroAdrian/Assets/Scripts/UI/UIMainMenu.cs
@@ -10,16 +10,47 @@
         public class UIMainMenu : MonoBehaviour
         {
 
+            [SerializeField] List<UIAnimationComponent> splashArts = null;
+            [Space(10)]
             [SerializeField] UIAnimationComponent mainMenuAnimationComponent = null;
             [SerializeField] UIAnimationComponent creditsAnimationComponent = null;
             [SerializeField] float transitionSpeed = .2f;
+
+            int currentSplashArt = 0;
 
             // Start is called before the first frame update
             void Start()
             {
                 mainMenuAnimationComponent.SetTransitionSpeed(transitionSpeed);
                 creditsAnimationComponent.SetTransitionSpeed(transitionSpeed);
-                mainMenuAnimationComponent.TransitionIn();
+                if(splashArts.Count > 0)
+                {
+                    foreach (var item in splashArts)
+                    {
+                        item.OnTransitionEnd += NextSplashArt;
+                    }
+                    splashArts[0].TransitionIn();
+                }
+                else
+                {
+                    SceneManager.Get().WhiteScreenUnfade();
+                    mainMenuAnimationComponent.TransitionIn();
+                }
+            }
+
+
+            void NextSplashArt()
+            {
+                currentSplashArt++;
+                if (currentSplashArt < splashArts.Count)
+                {
+                    splashArts[currentSplashArt].TransitionIn();
+                }
+                else
+                {
+                    SceneManager.Get().WhiteScreenUnfade();
+                    mainMenuAnimationComponent.TransitionIn();
+                }
             }
 
             public void OpenCredits()
@@ -37,6 +68,7 @@
             public void PlayGame()
             {
                 mainMenuAnimationComponent.TransitionOut();
+                SceneManager.Get().LoadSceneAsync("Game Scene");
             }
 
             public void ExitGame()
