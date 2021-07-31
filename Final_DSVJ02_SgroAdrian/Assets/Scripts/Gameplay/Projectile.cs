2aa76992038c7e53a811f6ebd1759575a64c38b4
Adrian <Owinmowe@gmail.com>
2021-07-31 17:53:39 -0300

    Added projectile on impact particles.

--- Final_DSVJ02_SgroAdrian/Assets/Scripts/Gameplay/Projectile.cs
+++ Final_DSVJ02_SgroAdrian/Assets/Scripts/Gameplay/Projectile.cs
@@ -6,16 +6,21 @@ namespace MarsArena
     [RequireComponent(typeof(Rigidbody))]
     public class Projectile : MonoBehaviour
     {
-        [SerializeField] List<Renderer> rendersToColorize;
+        [SerializeField] List<Renderer> rendersToColorize = null;
+        [Header("On Impact")]
+        [SerializeField] GameObject particlesGO;
+        [SerializeField] float timeToDestroy = 5f;
         float contactDamage = 1;
-        Rigidbody rb;
+        Rigidbody rb = null;
+        Collider col = null;
 
         public void SetProjectile(float damage, int layer, Color projColor)
         {
             gameObject.layer = layer;
             contactDamage = damage;
             rb = GetComponent<Rigidbody>();
-            foreach(Renderer r in rendersToColorize)
+            col = GetComponent<Collider>();
+            foreach (Renderer r in rendersToColorize)
             {
                 r.material.SetColor("_EmissionColor", projColor);
             }
@@ -23,18 +28,21 @@ namespace MarsArena
 
         public void Launch(Vector3 normalizedDir, float speed)
         {
-
             rb.AddForce(normalizedDir * speed);
         }
 
         private void OnCollisionEnter(Collision collision)
         {
+            rb.velocity = Vector3.zero;
+            col.enabled = false;
             IDamageable damageComponent = collision.collider.GetComponent<IDamageable>();
             if(damageComponent != null)
             {
                 damageComponent.TakeDamage(contactDamage);
             }
-            Destroy(gameObject);
+            particlesGO.SetActive(true);
+            particlesGO.transform.up = collision.GetContact(0).normal;
+            Destroy(gameObject, timeToDestroy);
         }
 
     }
