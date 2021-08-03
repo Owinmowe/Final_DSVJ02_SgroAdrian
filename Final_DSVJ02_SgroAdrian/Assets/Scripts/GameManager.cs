bf025971a83ce660c07fb66a198772629446522d
Adrian <Owinmowe@gmail.com>
2021-08-03 18:35:37 -0300

    Added engame events.

--- Final_DSVJ02_SgroAdrian/Assets/Scripts/GameManager.cs
+++ Final_DSVJ02_SgroAdrian/Assets/Scripts/GameManager.cs
@@ -9,8 +9,10 @@
     public class GameManager : MonoBehaviour
     {
 
+        [Header("General Gameplay")]
         [SerializeField] Terrain marsTerrain = null;
         [SerializeField] TankMovement playerTank = null;
+        [SerializeField] float maxTimePerSession = 200f;
 
         [Header("Pylons")]
         [SerializeField] GameObject pylonPrefab = null;
@@ -34,14 +36,25 @@
         [SerializeField] float turretsMaxDistanceSpawn = 450;
 
         public Action OnPlayerDestroyed;
+        public Action OnTimeUp;
+        public Action<bool> OnPlayerPressedPause;
+        public Action<float> OnTimeChanged;
         public Action<float> OnPlayerMoved;
         public Action<int> OnEnemyDestroyed;
         public Action<float, float> OnPlayerLifeChanged;
 
+        bool gamePaused = false;
+        int playerTotalPoints = 0;
+        int playerPylonsDestroyed = 0;
+        float distanceMoved = 0;
+        float currentTime = 0;
+
         // Start is called before the first frame update
         void Start()
         {
+            Time.timeScale = 0;
             playerTank.OnMove += PlayerMoved;
+            playerTank.GetComponent<PlayerInput>().OnPausedGame = TogglePause;
             var playerDestructableComponent = playerTank.GetComponent<DestructableComponent>();
             playerDestructableComponent.OnDestroy += PlayerDestroyed;
             playerDestructableComponent.OnLifeChanged += PlayerLifeChanged;
@@ -54,33 +67,43 @@
 
             for (int i = 0; i < startingTanksAmount; i++)
             {
-                CreateEnemyTank();
+                CreateEnemyWithAi(enemyTanksPrefab, tanksParent, tanksMaxDistanceSpawn, tanksGroundOffset);
             }
 
             for (int i = 0; i < startingTurretsAmount; i++)
             {
-                CreateEnemyTurret();
+                CreateEnemyWithAi(enemyTurretsPrefab, turretsParent, turretsMaxDistanceSpawn, turretsGroundOffset);
             }
         }
 
+        private void Update()
+        {
+            if (!gamePaused)
+            {
+                currentTime += Time.deltaTime;
+                OnTimeChanged?.Invoke(currentTime);
+                if(currentTime > maxTimePerSession)
+                {
+                    TimeIsUp();
+                }
+            }
+        }
 
         private void CreatePylon()
         {
             GameObject go = CreateEntity(pylonPrefab, pylonParent, pylonMaxDistanceSpawn, pylonGroundOffset);
-            go.GetComponent<PylonAI>().PylonDestroyed += EnemyDestroyed;
+            go.GetComponent<PylonAI>().OnDestroy += EnemyDestroyed;
+            go.GetComponent<PylonAI>().OnDestroy += PylonDestroyed;
         }
 
-        private void CreateEnemyTank()
+        private void CreateEnemyWithAi(GameObject prefab, Transform parent, float spawnDistance, float groundOffset)
         {
-            GameObject go = CreateEntity(enemyTanksPrefab, tanksParent, tanksMaxDistanceSpawn, tanksGroundOffset);
-            //go.GetComponent<Tank>().OnDestroy += EnemyDestroyed;
-        }
-
-        private void CreateEnemyTurret()
-        {
-            GameObject go = CreateEntity(enemyTurretsPrefab, turretsParent, turretsMaxDistanceSpawn, turretsGroundOffset);
-            go.GetComponent<TurretAI>().SetPlayerTransform(playerTank.transform);
-            //go.GetComponent<Tank>().OnDestroy += EnemyDestroyed;
+            GameObject go = CreateEntity(prefab, parent, spawnDistance, groundOffset);
+            var EnemyAi = go.GetComponent<AggresiveEnemyAI>();
+            EnemyAi.SetPlayerTransform(playerTank.transform);
+            EnemyAi.OnDestroy += EnemyDestroyed;
+            OnPlayerDestroyed += EnemyAi.StopAttack;
+            OnTimeUp += EnemyAi.StopAttack;
         }
 
         GameObject CreateEntity(GameObject prefab, Transform parent, float maxSpawnDis, float groundOffset = 0)
@@ -97,16 +120,31 @@
             return go;
         }
 
+        void PylonDestroyed(int points)
+        {
+            playerPylonsDestroyed++;
+        }
+
         void EnemyDestroyed(int pointsGot)
         {
-            OnEnemyDestroyed?.Invoke(pointsGot);
+            playerTotalPoints += pointsGot;
+            OnEnemyDestroyed?.Invoke(playerTotalPoints);
         }
 
         void PlayerDestroyed()
         {
+            SceneManager.Get().SetLastSessionArenaData(false, playerTotalPoints, playerPylonsDestroyed, distanceMoved);
             OnPlayerDestroyed?.Invoke();
         }
         
+        void TimeIsUp()
+        {
+            SceneManager.Get().SetLastSessionArenaData(true, playerTotalPoints, playerPylonsDestroyed, distanceMoved);
+            gamePaused = true;
+            StopGame();
+            OnTimeUp?.Invoke();
+        }
+
         void PlayerLifeChanged(float armor, float shield)
         {
             OnPlayerLifeChanged?.Invoke(armor, shield);
@@ -114,7 +152,36 @@
 
         void PlayerMoved(float moveAmount)
         {
+            distanceMoved = moveAmount;
             OnPlayerMoved?.Invoke(moveAmount);
+        }
+
+        void TogglePause()
+        {
+            gamePaused = !gamePaused;
+            OnPlayerPressedPause?.Invoke(gamePaused);
+            if (gamePaused) Time.timeScale = 0;
+            else Time.timeScale = 1;
+        }
+
+        public void GoBackToMenu()
+        {
+            SceneManager.Get().LoadSceneAsync("Main Menu");
+        }
+
+        public void GoToEndGameScene()
+        {
+            SceneManager.Get().LoadSceneAsync("End Game");
+        }
+
+        public void StartGame()
+        {
+            Time.timeScale = 1;
+        }
+
+        void StopGame()
+        {
+            Time.timeScale = 0;
         }
 
     }
