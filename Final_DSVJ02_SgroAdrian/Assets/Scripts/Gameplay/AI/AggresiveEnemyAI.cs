b23ac46aef2d1ce7f0338cef6e2756d5d4b96163
Adrian <Owinmowe@gmail.com>
2021-08-03 18:35:08 -0300

    Improved Enemy AI reused code using Inheritance.

---
+++ Final_DSVJ02_SgroAdrian/Assets/Scripts/Gameplay/AI/AggresiveEnemyAI.cs
@@ -0,0 +1,63 @@
+﻿namespace MarsArena
+{
+
+    using System;
+    using UnityEngine;
+
+    [RequireComponent(typeof(DestructableComponent))]
+    public class AggresiveEnemyAI : MonoBehaviour
+    {
+        [Header("General Gameplay")]
+        [SerializeField] int pointsOnDestroy = 100;
+        [Header("AI Behaviour")]
+        [SerializeField] float distanceToAttackPlayer = 25f;
+        [SerializeField] float distanceToMoveToPlayer = 50f;
+
+        protected Transform playerTrans = null;
+        protected DestructableComponent destructableComponent;
+
+        public Action<int> OnDestroy;
+
+        protected bool locked = false;
+
+        protected virtual void Awake()
+        {
+            SetAIBasicComponents();
+        }
+
+        void SetAIBasicComponents()
+        {
+            destructableComponent = GetComponent<DestructableComponent>();
+            destructableComponent.OnDestroy += Destroy;
+        }
+
+        protected bool PlayerInMoveDistance()
+        {
+            Vector3 offset = playerTrans.position - transform.position;
+            return offset.sqrMagnitude < distanceToMoveToPlayer * distanceToMoveToPlayer;
+        }
+
+        protected bool PlayerInAttackDistance()
+        {
+            Vector3 offset = playerTrans.position - transform.position;
+            return offset.sqrMagnitude < distanceToAttackPlayer * distanceToAttackPlayer;
+        }
+
+        public void SetPlayerTransform(Transform playerTransform)
+        {
+            playerTrans = playerTransform;
+        }
+
+        public void StopAttack()
+        {
+            locked = true;
+        }
+
+        private void Destroy()
+        {
+            StopAttack();
+            OnDestroy?.Invoke(pointsOnDestroy);
+        }
+
+    }
+}
